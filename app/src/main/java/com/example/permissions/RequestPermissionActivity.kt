5d1bfe3417c55900f68fe817931649a3a27e536e
Alyona <borisovsk_a@mail.ru>
2023-02-15 15:20:49 +0300

    feat: onRequestPermission working

--- app/src/main/java/com/example/permissions/RequestPermissionActivity.kt
+++ app/src/main/java/com/example/permissions/RequestPermissionActivity.kt
@@ -1,11 +1,88 @@
 package com.example.permissions
 
+import android.Manifest
+import android.Manifest.permission.CAMERA
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 
 class RequestPermissionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_request_permission)
+
+        checkCameraPermission()
+    }
+
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            // Granted
+            // TODO snackbar
+            Toast.makeText(this, "Camera permission is already granted", Toast.LENGTH_SHORT).show()
+        } else {
+            // Not granted
+            requestCameraPermission()
+        }
+    }
+
+    private fun requestCameraPermission() {
+        Log.d(TAG, "In requestCameraPerm")
+        requestPermissions(arrayOf(CAMERA),
+            CAMERA_PERM_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d(TAG, "In onRequestPermissionResult")
+
+        if (requestCode == CAMERA_PERM_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            ) {
+                // FIXME сюда не переходит!
+                Log.d(TAG, "Camera permitted")
+                updateStatus()
+
+            } else {
+                // show alert dialog
+                Log.d(TAG, "Camera not permitted, before showing alert dialog")
+                //
+                AlertDialog.Builder(this).setTitle("Alert")
+                    .setMessage("We really need camera permission.")
+                    .setPositiveButton("OK") { _, _ ->
+                        requestCameraPermission()
+                    }
+                    .setNegativeButton("NO") { _, _ ->
+                        Log.d(TAG, "Camera permission is still DENIED")
+                        Toast.makeText(this, "Camera permission is still DENIED", Toast.LENGTH_SHORT).show()
+                    }
+                    .show()
+            }
+            return
+        }
+    }
+
+    private fun updateStatus() {
+        if (ContextCompat.checkSelfPermission(this, CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Camera permission is granted now")
+            Toast.makeText(this, "Camera permission is granted now", Toast.LENGTH_SHORT).show()
+        } else {
+            Log.d(TAG, "Camera permission is DENIED now")
+            Toast.makeText(this, "Camera permission is DENIED now", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        const val TAG = "ReqCamPermAct"
+        const val CAMERA_PERM_REQUEST_CODE = 101
     }
 }
