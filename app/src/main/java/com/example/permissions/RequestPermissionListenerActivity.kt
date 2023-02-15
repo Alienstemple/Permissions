9b30333fcfc812fe1c6ea91560c7fefb2180ce34
Alyona <borisovsk_a@mail.ru>
2023-02-15 17:30:18 +0300

    debug: actResLauncher

--- app/src/main/java/com/example/permissions/RequestPermissionListenerActivity.kt
+++ app/src/main/java/com/example/permissions/RequestPermissionListenerActivity.kt
@@ -1,22 +1,23 @@
 package com.example.permissions
 
 import android.Manifest.permission.CAMERA
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 
 class RequestPermissionListenerActivity : AppCompatActivity() {
 
     private val activityResultLauncher =
         registerForActivityResult(
-            ActivityResultContracts.RequestPermission()){ isGranted ->
+            ActivityResultContracts.RequestPermission()) { isGranted ->
             // Handle Permission granted/rejected
             if (isGranted) {
                 Log.d(TAG, "Camera permission granted")
             } else {
                 // Permission is denied
-                // FIXME не приходит сюда
                 Log.d(TAG, "Camera permission denied")
             }
         }
@@ -26,6 +27,33 @@ class RequestPermissionListenerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_request_permission_listener)
 
         activityResultLauncher.launch(CAMERA)
+
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.d(TAG, "Camera permission granted")
+            }
+            shouldShowRequestPermissionRationale()
+            -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected, and what
+                // features are disabled if it's declined. In this UI, include a
+                // "cancel" or "no thanks" button that lets the user continue
+                // using your app without granting the permission.
+                showInContextUI(...)
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.REQUESTED_PERMISSION
+
+                )
+            }
+        }
+
     }
 
     companion object {
