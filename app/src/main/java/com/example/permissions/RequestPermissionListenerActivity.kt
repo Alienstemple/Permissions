f3fbeaf2ab554441160271d0103fdc03b57f447c
Alyona <borisovsk_a@mail.ru>
2023-02-16 10:31:25 +0300

    feat: actResLauncher working

--- app/src/main/java/com/example/permissions/RequestPermissionListenerActivity.kt
+++ app/src/main/java/com/example/permissions/RequestPermissionListenerActivity.kt
@@ -2,10 +2,13 @@ package com.example.permissions
 
 import android.Manifest.permission.CAMERA
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class RequestPermissionListenerActivity : AppCompatActivity() {
@@ -18,7 +21,19 @@ class RequestPermissionListenerActivity : AppCompatActivity() {
                 Log.d(TAG, "Camera permission granted")
             } else {
                 // Permission is denied
-                Log.d(TAG, "Camera permission denied")
+                // launch alert dialog
+                // Проверяем, не стоит ли в настройках "запретить"
+                // Иначе shouldShow всегда будет false
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, CAMERA)) {
+                    // AlertDialog
+                    Log.d(TAG, "Camera not permitted after request, before showing alert dialog")
+                    showAlertDialog()
+                } else {
+                    Log.d(TAG,
+                        "Should NOT reqPermRation")
+                    Toast.makeText(this, "Camera permission is still DENIED", Toast.LENGTH_SHORT)
+                        .show()
+                }
             }
         }
 
@@ -26,34 +41,31 @@ class RequestPermissionListenerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_request_permission_listener)
 
-        activityResultLauncher.launch(CAMERA)
-
-        when {
-            ContextCompat.checkSelfPermission(
-                this,
-                CAMERA
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                Log.d(TAG, "Camera permission granted")
-            }
-            shouldShowRequestPermissionRationale()
-            -> {
-                // In an educational UI, explain to the user why your app requires this
-                // permission for a specific feature to behave as expected, and what
-                // features are disabled if it's declined. In this UI, include a
-                // "cancel" or "no thanks" button that lets the user continue
-                // using your app without granting the permission.
-                showInContextUI(...)
-            }
-            else -> {
-                // You can directly ask for the permission.
-                // The registered ActivityResultCallback gets the result of this request.
-                requestPermissionLauncher.launch(
-                    Manifest.permission.REQUESTED_PERMISSION
-
-                )
-            }
+        if (ContextCompat.checkSelfPermission(this, CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Camera permission already granted")
+        } else {
+            Log.d(TAG, "Camera not permitted, launching request first time")
+            activityResultLauncher.launch(CAMERA)
         }
+    }
 
+    private fun showAlertDialog() {
+        // show alert dialog
+        AlertDialog.Builder(this).setTitle("Alert")
+            .setMessage("We really need camera permission.")
+            .setPositiveButton("OK") { _, _ ->
+
+                Log.d(TAG, "Alert Dialog OK, before launching request")
+                activityResultLauncher.launch(CAMERA)
+
+            }
+            .setNegativeButton("NO") { _, _ ->
+
+                Log.d(TAG, "Alert Dialog NO, before launching request")
+                Toast.makeText(this, "Camera permission is still DENIED", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            .show()
     }
 
     companion object {
